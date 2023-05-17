646ddf41966f8f8835016c4f55e2f1498397f09e
srtapacheco <karenpacheco16@poli.ufrj.br>
2023-05-16 22:37:22 -0300

    Versao do codigo em C que consegue ultrapassar o limite de matrizes de tamanho 1000 via malloc

--- arquivos_C/main.c
+++ arquivos_C/main.c
@@ -5,7 +5,7 @@
 #include <stdbool.h>
 #include <unistd.h>
 
-// Função para medir o tempo de execução
+// Função para medir o tempo de execucao
 double get_execution_time(struct timespec start, struct timespec end)
 {
     double t;
@@ -19,40 +19,55 @@ double get_execution_time(struct timespec start, struct timespec end)
 int main()
 {
     // Inicialização de variáveis
-    int n_min = 10;      // Tamanho mínimo do problema
-    int n_max = 1000;    // Tamanho máximo do problema
-    int n_step = 10;     // Tamanho do passo para aumentar o tamanho do problema
-    double A[n_max][n_max], x[n_max], b[n_max];  // Matrizes A, x e b
-    int i, j, k, n;      // Contadores de loop
-    double start, end, elapsed; // Variáveis para medir o tempo de execução
+    int n_min = 10;             // Tamanho mínimo do problema
+    int n_max = 2000;           // Tamanho máximo do problema
+    int n_step = 10;            // Tamanho do passo para aumentar o tamanho do problema
+    double **A, *x, *b;         // Matrizes A, x e b
+    int i, j, k, n;             // Contadores de loop
+    double start, end, elapsed; // Variáveis para medir o tempo de execucao
+
+    // Alocando memória para as matrizes A, x e b
+    A = (double **)malloc(n_max * sizeof(double *));
+    for (i = 0; i < n_max; i++)
+    {
+        A[i] = (double *)malloc(n_max * sizeof(double));
+    }
+    x = (double *)malloc(n_max * sizeof(double));
+    b = (double *)malloc(n_max * sizeof(double));
 
     // Preenchimento das matrizes A e x com números aleatórios
     srand(time(NULL));
-    for (i = 0; i < n_max; i++) {
+    for (i = 0; i < n_max; i++)
+    {
         x[i] = (double)rand() / (double)RAND_MAX;
-        for (j = 0; j < n_max; j++) {
+        for (j = 0; j < n_max; j++)
+        {
             A[i][j] = (double)rand() / (double)RAND_MAX;
         }
     }
 
-    // Loop para aumentar o tamanho do problema e medir o tempo de execução
+    // Loop para aumentar o tamanho do problema e medir o tempo de execucao
     FILE *gp = popen("gnuplot -persist", "w");
     fprintf(gp, "set xlabel 'Tamanho do problema'\n");
-    fprintf(gp, "set ylabel 'Tempo de execução (s)'\n");
-    fprintf(gp, "plot '-' title 'Tempo de execução' with linespoints\n");
-    for (n = n_min; n <= n_max; n += n_step) {
+    fprintf(gp, "set ylabel 'Tempo de execucao (s)'\n");
+    fprintf(gp, "plot '-' title 'Tempo de execucao' with linespoints\n");
+    for (n = n_min; n <= n_max; n += n_step)
+    {
         // Preenchimento da matriz b com zeros
-        for (i = 0; i < n; i++) {
+        for (i = 0; i < n; i++)
+        {
             b[i] = 0.0;
         }
 
-        // Medição do tempo de execução
+        // Medição do tempo de execucao
         struct timespec start_time, end_time;
         clock_gettime(CLOCK_MONOTONIC_RAW, &start_time);
 
         // Cálculo do produto matriz-vetor
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < n; j++) {
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
                 b[i] += A[i][j] * x[j];
             }
         }
@@ -60,11 +75,20 @@ int main()
         clock_gettime(CLOCK_MONOTONIC_RAW, &end_time);
         elapsed = get_execution_time(start_time, end_time);
 
-        // Impressão do tempo de execução
+        // Impressão do tempo de execucao
         printf("%d %f\n", n, elapsed);
         fprintf(gp, "%d %f\n", n, elapsed);
     }
     fprintf(gp, "e\n");
+
+    // Liberação da memória alocada
+    for (i = 0; i < n_max; i++)
+    {
+        free(A[i]);
+    }
+    free(A);
+    free(x);
+    free(b);
 
     // Fechamento do pipe para o gnuplot
     pclose(gp);
